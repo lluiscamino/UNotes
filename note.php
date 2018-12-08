07eac17c8157cbe5f3fbc0a63bff2ffebe191ed1
Lluís Camino <lluiscamino@users.noreply.github.com>
2018-12-08 17:04:36 +0100

    fix note.php
    display the right files in a note

--- note.php
+++ note.php
@@ -5,7 +5,7 @@ require 'util/includes.php';
 try {
     $note = new Note($mysqli, (int)$_GET['noteid']);
     $note->addView();
-    echo $templates->render('pages::note', array('valueArray'  =>  $note->getValueArray() , 'files' => File::noteFiles($mysqli, 1)));
+    echo $templates->render('pages::note', array('valueArray'  =>  $note->getValueArray() , 'files' => File::noteFiles($mysqli, (int)$_GET['noteid'])));
 } catch(Exception $e) {
     echo $templates->render('pages::nonote', array());
 }
