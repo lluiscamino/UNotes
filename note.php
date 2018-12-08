5268cb54ab74429b92430ba73a5a2d57624b3e10
Lluís Camino <lluiscamino@users.noreply.github.com>
2018-12-08 17:30:29 +0100

    add author_name to notes

--- note.php
+++ note.php
@@ -1,11 +1,15 @@
 <?php
 use src\Note;
 use src\File;
+use src\User;
 require 'util/includes.php';
 try {
     $note = new Note($mysqli, (int)$_GET['noteid']);
     $note->addView();
-    echo $templates->render('pages::note', array('valueArray'  =>  $note->getValueArray() , 'files' => File::noteFiles($mysqli, (int)$_GET['noteid'])));
+    $valueArray = $note->getValueArray();
+    $user = new User($mysqli, $valueArray['author_id']);
+    $userDetails = $user->exists() ? array_merge($user->getValuesArray(), array('isguest' => false)) : array('isguest' => true);
+    echo $templates->render('pages::note', array('valueArray'  =>  $valueArray , 'userDetails'  =>  $userDetails, 'files' => File::noteFiles($mysqli, (int)$_GET['noteid'])));
 } catch(Exception $e) {
     echo $templates->render('pages::nonote', array());
 }
--- resources/templates/pages/note.php
+++ resources/templates/pages/note.php
@@ -1,21 +1,25 @@
-<?php $this->layout('global::template', array(
-    'subtitle'  =>  $this->e($valueArray['title']),
-    'navigation'    =>  $this->e($this->getTr('INDEX')) . ', ' . $this->e($this->getTr('NOTES')) .', ' . $this->e($valueArray['title']),
-    'navigationLinks'   =>  'index, index'
-    ))?>
+<?php
+$this->layout('global::template', array(
+'subtitle'  =>  $this->e($valueArray['title']),
+'navigation'    =>  $this->e($this->getTr('INDEX')) . ', ' . $this->e($this->getTr('NOTES')) .', ' . $this->e($valueArray['title']),
+'navigationLinks'   =>  'index, index'
+))?>
 <h2><?php 
 echo $this->e($valueArray['title']);
 // checks if note was published in the last 5 minutes
 if (time() - strtotime($valueArray['creation_date']) <= 300) {
     echo ' <span class="badge badge-success">New</span>';
 }
+$creationDate = new DateTime($this->e($valueArray['creation_date']));
+$userLink = $this->e($userDetails['isguest']) === true ? $this->e($this->getTr('GUEST')) : '<a href="profile/' . $this->e($userDetails['id']) . '" target="_blank">' . $this->e($userDetails['name']) . '</a>';
 ?></h2>
 <div class="card">
   <div class="card-body">
     <?php echo $this->e($valueArray['description']);?>
     <div id="note-info">
-      <img src="resources/images/icons/eye.png"> <?php echo $this->e($valueArray['num_views']) .  ' ' . $this->e($this->getTr('VIEWS'));?> 
-      <img src="resources/images/icons/calendar.png"> <?php echo date("d/m/Y h:i", strtotime($this->e($valueArray['creation_date'])));?>
+      <img src="resources/images/icons/userpencil.png"> <?php echo $userLink ;?> |
+      <img src="resources/images/icons/eye.png"> <?php echo $this->e($valueArray['num_views']) .  ' ' . $this->e($this->getTr('VIEWS'));?> |
+      <img src="resources/images/icons/calendar.png"> <?php echo $creationDate->format($this->e($this->getTr('DATE_FORMAT')));?>
     </div>
     <hr>
     <?php echo $this->e($this->getTr('SHARENOTES')); ?>: <input type="text" class="form-control" id="share-link" value="<?php echo $_SERVER['REQUEST_URI'];?>/notes/<?php echo $this->e($valueArray['id']);?>" readonly> <button type="button" class="btn btn-outline-primary" id="copy-button">Copy</button>
